4949e2661c13b47f3a2568c4a5be37fe6b76af95
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 22:48:53 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,10 @@
 require 'pry'
- 
- 
+
+
  class Game
- 
+
    attr_accessor :board, :player_1, :player_2
- 
+
    #define all winning combinations
    WIN_COMBINATIONS = [
      [0,1,2],
@@ -16,7 +16,7 @@ require 'pry'
      [0,4,8],
      [2,4,6]
    ]
- 
+
    #initializes player 1 and player 2 with defaults X and O.
    #initiales a new game board with default blank board
    def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
@@ -24,24 +24,24 @@ require 'pry'
      @player_2 = player_2
      @board = board
    end
- 
+
    #displays board
    def board
      @board
    end
- 
+
    #check for current player by seeing what turn_count is
    #if even, both players have gone so then it's player 1 against
    def current_player
      @board.turn_count.even? ? @player_1 : @player_2
- 
+
    end
- 
+
    #checks to see if the game has been won or if it's a draw
    def over?
      won? || draw?
    end
- 
+
    #looks through win_combinations for the first matching combo
    #if all combo cells are X, set @winner as X, and return the combo
    #if all combo cells are O, set @winner as O, and return the combo
@@ -58,24 +58,24 @@ require 'pry'
      end
      nil
    end
- 
+
    #check to see if it's a draw game by verifying board full?
    #to be a draw, the game must not be a won game.
    def draw?
      @board.full? && !won?
    end
- 
+
    #if the game is won, return the first cell in the winning combo
    #if not won, return nil
    def winner
      won? ? board.cells[won?[0]] : nil
    end
- 
+
    #grabs the current players board selection and store in input
    #if the move is valid, it updates the board with the move and player token.
    def turn
      input = current_player.move(board)
- 
+
      if board.valid_move?(input)
        board.update(input, current_player)
        board.display
@@ -83,8 +83,8 @@ require 'pry'
        turn
      end
    end
- 
- 
+
+
    #if the game isn't over, keep taking turns
    #over method verifies if the gave has been won or is a draw
    #after every turn verify if it's a won game or a draw game
@@ -93,6 +93,6 @@ require 'pry'
        turn
      end
      won? ? (puts "Congratulations #{@winner}!") : (puts "Cat's Game!")
- 
+
    end
  end
